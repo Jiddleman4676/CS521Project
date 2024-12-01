8ddbc631518883567872a07100877f45f6bde16e
Jeremy Middleman <jiddleman@gmail.com>
2024-12-01 16:26:59 -0700

    part of last commit

--- src/main.py
+++ src/main.py
@@ -42,54 +42,54 @@ for idx, column_name in enumerate(df.columns[1:]):  # Start from the second colu
     print(f"Unique values: {unique_values}")
     print("-" * 30)  # Separator for readability
 
-## Split data into train partition and test partition
-#X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0, test_size=0.95)
-#_, X_test, _, y_test = train_test_split(X_test, y_test, random_state=0, test_size=0.1)
-## Define the FNN model with 784 input neuron, 64 hidden neurons in 2 layers, and 10 output neuron
-#nn = FeedforwardNeuralNetwork(layer_sizes=layer_sizes, activations=[sigmoid, sigmoid,sigmoid,sigmoid])
-#
-#print("training")
-#print(f"Layers: {layer_sizes[1:-1]} N: {len(X_train)}, LR: {LEARNING_RATE}, E: {EPOCHS}")
-#start_time = time.time()  # Record start time
-#
-#nn.train(X_train, y_train, loss_function=NNN_loss,
-#         loss_derivative=lambda y_pred, y_true: NNN_loss(y_pred, y_true, derivative=True),
-#         epochs=EPOCHS, learning_rate=LEARNING_RATE)
-#
-#end_time = time.time()  # Record end time
-#training_duration = np.round((end_time - start_time) / 60)  # Calculate duration
-#
-#print("test")
-#
-## Evaluate model on test data
-#correct_counts = np.zeros(3)
-#incorrect_counts = np.zeros(3)
-#
-#for i in range(len(y_test)):
-#    y_pred = nn.forward(X_test[i])
-#    if np.random.randint(1, 1000) == 1:
-#        print(y_pred)
-#    if np.argmax(y_pred)  ==  y_test[i]:
-#        correct_counts[y_test[i]] += 1
-#    else:
-#        incorrect_counts[y_test[i]] += 1
-#
-#print("correct: ", np.sum(correct_counts), "incorrect: ", np.sum(incorrect_counts))
-#print("accuracy: ", np.sum(correct_counts) / (np.sum(correct_counts) + np.sum(incorrect_counts))) 
-#
-#covtype = np.arange(3)
-#
-## Plotting correct vs incorrect classifications for each digit
-#plt.figure(figsize=(3, 6)) #??
-#plt.bar(covtype, correct_counts, label="Correct", color="blue")
-#plt.bar(covtype, incorrect_counts, bottom=correct_counts, label="Incorrect", color="red")
-#
-## Labeling the plot
-#plt.xlabel("Diabetic Status")
-#plt.ylabel("Number of Classifications")
-#plt.title(f"Correct vs Incorrect | Layers: {layer_sizes[1:-1]} N: {len(X_train)}, LR: {LEARNING_RATE}, E: {EPOCHS}, T:{training_duration} min,Correct: {((np.sum(correct_counts)/len(y_test)) * 100):.2f}%")
-#plt.xticks(covtype)
-#plt.legend()
-#
-## Show plot
-#plt.show()
+# Split data into train partition and test partition
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0, test_size=0.95)
+_, X_test, _, y_test = train_test_split(X_test, y_test, random_state=0, test_size=0.1)
+# Define the FNN model with 784 input neuron, 64 hidden neurons in 2 layers, and 10 output neuron
+nn = FeedforwardNeuralNetwork(layer_sizes=layer_sizes, activations=[sigmoid, sigmoid,sigmoid,sigmoid])
+
+print("training")
+print(f"Layers: {layer_sizes[1:-1]} N: {len(X_train)}, LR: {LEARNING_RATE}, E: {EPOCHS}")
+start_time = time.time()  # Record start time
+
+nn.train(X_train, y_train, loss_function=NNN_loss,
+         loss_derivative=lambda y_pred, y_true: NNN_loss(y_pred, y_true, derivative=True),
+         epochs=EPOCHS, learning_rate=LEARNING_RATE)
+
+end_time = time.time()  # Record end time
+training_duration = np.round((end_time - start_time) / 60)  # Calculate duration
+
+print("test")
+
+# Evaluate model on test data
+correct_counts = np.zeros(3)
+incorrect_counts = np.zeros(3)
+
+for i in range(len(y_test)):
+    y_pred = nn.forward(X_test[i])
+    if np.random.randint(1, 1000) == 1:
+        print(y_pred)
+    if np.argmax(y_pred)  ==  y_test[i]:
+        correct_counts[y_test[i]] += 1
+    else:
+        incorrect_counts[y_test[i]] += 1
+
+print("correct: ", np.sum(correct_counts), "incorrect: ", np.sum(incorrect_counts))
+print("accuracy: ", np.sum(correct_counts) / (np.sum(correct_counts) + np.sum(incorrect_counts))) 
+
+covtype = np.arange(3)
+
+# Plotting correct vs incorrect classifications for each digit
+plt.figure(figsize=(3, 6)) #??
+plt.bar(covtype, correct_counts, label="Correct", color="blue")
+plt.bar(covtype, incorrect_counts, bottom=correct_counts, label="Incorrect", color="red")
+
+# Labeling the plot
+plt.xlabel("Diabetic Status")
+plt.ylabel("Number of Classifications")
+plt.title(f"Correct vs Incorrect | Layers: {layer_sizes[1:-1]} N: {len(X_train)}, LR: {LEARNING_RATE}, E: {EPOCHS}, T:{training_duration} min,Correct: {((np.sum(correct_counts)/len(y_test)) * 100):.2f}%")
+plt.xticks(covtype)
+plt.legend()
+
+# Show plot
+plt.show()
